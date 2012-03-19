2b6071eda8f227f0380baffc397c749d53f0c726
Tim Muller <t_muller@hotmail.com>
2012-03-19 11:32:03 +0100

    second test refactor (green zone)

--- src/TennisGameTest.java
+++ src/TennisGameTest.java
@@ -2,22 +2,33 @@
 import junit.framework.TestCase;
 
 public class TennisGameTest extends TestCase{
+	
+	private TennisGame t;
+	
+	protected void setUp() throws Exception{
+		t = new TennisGame();
+	}
+	
 	public void testLoveSet(){
-		TennisGame t = new TennisGame();
-		for (int i = 0; i < 24; i++){
-			t.wonGame(false);
-		}
+		int games = 24;
+		boolean wonAllGames = false;
+		this.playGames(games, wonAllGames);
 		int score = t.getWonGames();
 		assertEquals(0, score);
 	}
 	
 	public void testWonSet(){
-		TennisGame t = new TennisGame();
-		for(int i = 0; i < 24; i++){
-			t.wonGame(true);
-		}
+		int games = 24;
+		boolean wonAllGames = true;
+		this.playGames(games, wonAllGames);
 		int score = t.getWonGames();
 		assertEquals(24, score);
+	}
+	
+	public void playGames(int total, boolean result){
+		for(int i = 0; i < total; i++){
+			t.wonGame(result);
+		}
 	}
 	
 }
